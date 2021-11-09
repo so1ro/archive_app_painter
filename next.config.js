48eeae9cd5764bb37080d1485da0e0d728bea546
Masamichi Kagaya <masamichi.kagaya@gmail.com>
2021-11-10 08:33:57 +0900

    Add PWA

--- next.config.js
+++ next.config.js
@@ -1,29 +1,29 @@
 // with SW (Service Worker)
 const withPWA = require('next-pwa')
 
-// module.exports = withPWA({
-//     images: {
-//         domains: ['images.ctfassets.net', 'pbs.twimg.com', 'abs.twimg.com', 'i.ytimg.com'],
-//     },
-//     i18n: {
-//         locales: ['en', 'ja'],
-//         defaultLocale: 'en',
-//     },
-//     pwa: {
-//         dest: 'public'
-//     },
-// })
-
-// Develop without SW (Service Worker)
-module.exports = {
+module.exports = withPWA({
     images: {
         domains: ['images.ctfassets.net', 'pbs.twimg.com', 'abs.twimg.com', 'i.ytimg.com'],
     },
     i18n: {
         locales: ['en', 'ja'],
-        defaultLocale: 'en',
+        defaultLocale: 'ja',
     },
-}
+    pwa: {
+        dest: 'public'
+    },
+})
+
+// Develop without SW (Service Worker)
+// module.exports = {
+//     images: {
+//         domains: ['images.ctfassets.net', 'pbs.twimg.com', 'abs.twimg.com', 'i.ytimg.com'],
+//     },
+//     i18n: {
+//         locales: ['en', 'ja'],
+//         defaultLocale: 'ja',
+//     },
+// }
 
 // Twitter : 'pbs.twimg.com'
 // Contentful : 'images.ctfassets.net'
